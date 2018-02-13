6d9c7a0171841c730cbc4ae50875e5cde52a678f
Mamert Stankiewicz <mamert@oke.pl>
2018-02-13 09:09:21 +0100

    air mouse: offsets for "03" module

--- mouse/mousuino/hw_calibration.ino
+++ mouse/mousuino/hw_calibration.ino
@@ -4,11 +4,20 @@
 
 
 void mpu6050_setOffsets(MPU6050 accelgyro) {
-	// for module marked "02"
-    accelgyro.setXAccelOffset(-958);
-    accelgyro.setYAccelOffset(-5752);
-    accelgyro.setZAccelOffset(1795);
-    accelgyro.setXGyroOffset(177);
-    accelgyro.setYGyroOffset(11);
-    accelgyro.setZGyroOffset(-17);
+    // for module marked "02"
+//    accelgyro.setXAccelOffset(-958);
+//    accelgyro.setYAccelOffset(-5752);
+//    accelgyro.setZAccelOffset(1795);
+//    accelgyro.setXGyroOffset(177);
+//    accelgyro.setYGyroOffset(11);
+//    accelgyro.setZGyroOffset(-17);
+
+    // for module marked "03"
+    accelgyro.setXAccelOffset(2119);
+    accelgyro.setYAccelOffset(-3135);
+    accelgyro.setZAccelOffset(909);
+    accelgyro.setXGyroOffset(-251);
+    accelgyro.setYGyroOffset(-15);
+    accelgyro.setZGyroOffset(-20);
+
 }
