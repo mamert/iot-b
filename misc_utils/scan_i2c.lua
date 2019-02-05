64326da90a973fa4db7e6175d2791654dac9e978
Kaay <kaay.null@gmail.com>
2019-02-05 13:16:52 +0100

    scan_i2c: also check reversed pins

--- misc_utils/scan_i2c.lua
+++ misc_utils/scan_i2c.lua
@@ -3,24 +3,24 @@
 
 id=0 -- ALWAYS 0
 
-function testAllAddresses()
+function testAllAddresses(sda,scl)
+	i2c.setup(id,sda,scl,i2c.SLOW)
 	local found = false
 	for i=0,127 do
 		i2c.start(id)
 		resCode = i2c.address(id, i, i2c.TRANSMITTER)
 		i2c.stop(id)
 		if resCode == true then
-			print("We have a device on address 0x" .. string.format("%02x", i) .. " (" .. i ..")")
+			print("We have a device on address 0x" .. string.format("%02x", i) .. " (" .. i .."), sda="..sda..", scl="..scl)
 			found = true
 		end
 	end
-	if(not found) then print("Nothing found") end
+	return found
 end
 
 
-sda=2
-scl=4
 
-i2c.setup(id,sda,scl,i2c.SLOW)
-testAllAddresses()
+found = testAllAddresses(2,4)
+found = testAllAddresses(4,2) or found
+if(not found) then print("Nothing found") end
 
