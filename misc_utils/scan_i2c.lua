5288ef349beee5e97303d885b2783fdda5b33466
Kaay <kaay.null@gmail.com>
2019-02-05 12:58:04 +0100

    scan_i2c: function call, tell if nothing found

--- misc_utils/scan_i2c.lua
+++ misc_utils/scan_i2c.lua
@@ -1,17 +1,26 @@
--- http://www.esp8266.com/viewtopic.php?f=19&t=771
+-- based on http://www.esp8266.com/viewtopic.php?f=19&t=771
 -- Scan for I2C devices
 
-id=0
+id=0 -- ALWAYS 0
+
+function testAllAddresses()
+	local found = false
+	for i=0,127 do
+		i2c.start(id)
+		resCode = i2c.address(id, i, i2c.TRANSMITTER)
+		i2c.stop(id)
+		if resCode == true then
+			print("We have a device on address 0x" .. string.format("%02x", i) .. " (" .. i ..")")
+			found = true
+		end
+	end
+	if(not found) then print("Nothing found") end
+end
+
+
 sda=2
 scl=4
 
--- initialize i2c, set pin1 as sda, set pin0 as scl
-
 i2c.setup(id,sda,scl,i2c.SLOW)
+testAllAddresses()
 
-for i=0,127 do
-  i2c.start(id)
-  resCode = i2c.address(id, i, i2c.TRANSMITTER)
-  i2c.stop(id)
-  if resCode == true then print("We have a device on address 0x" .. string.format("%02x", i) .. " (" .. i ..")") end
-end
