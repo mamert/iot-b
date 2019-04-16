1ac9f20313840d06f493538726c4dab97f7fcb88
Kaay <kaay.null@gmail.com>
2019-04-16 19:17:46 +0200

    WIP mousuino interrupt (not working, figure out why)

--- mouse/mousuino/mousuino.ino
+++ mouse/mousuino/mousuino.ino
@@ -9,10 +9,13 @@
 #endif
 
 MPU6050 accelgyro; // default is 0x68; with AD0 high it's 0x69
-//int interruptPin = 3; // INT0 is 3 on Micro, 2 on others
+
+#define INTERRUPT_PIN 7
+// INT0 is pin 2 on most ATMEGAs, 3 on ATmega32u4, but 3 is taken up by SCL there
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+bool isInterrupted;
 
 int16_t sensitivityDivider = 250;
 //int deadZone = 40; // radius in which motion is ignored. Not used since much smaller than sensitivityDivider.
@@ -26,6 +29,14 @@ const char mouseBtn[] = {MOUSE_LEFT, MOUSE_MIDDLE, MOUSE_RIGHT};
 Bounce * btn = new Bounce[3];
 
 
+bool blinkState = false;
+
+void onMotion() {
+//    Serial.println("INTERRUPT");
+    isInterrupted = true;
+}
+
+
 void setup() {
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
@@ -37,7 +48,8 @@ void setup() {
     Serial.begin(38400);
 //    while (!Serial); // wait for serial port to connect. Needed for Leonardo only
     delay(10); // we don't really care about serial if not connected, so just this instead
-    
+
+    isInterrupted = false;
     Serial.println("Initializing MPU-6050");
     accelgyro.initialize();
 
@@ -47,6 +59,31 @@ void setup() {
     Serial.println("Updating internal sensor offsets...");
     mpu6050_setOffsets(accelgyro);
 
+    accelgyro.resetGyroscopePath();
+    accelgyro.resetAccelerometerPath();
+//    accelgyro.resetTemperaturePath();
+    accelgyro.setInterruptMode(1);// Register 37, Bit 7  0: active high
+    accelgyro.setInterruptDrive(0);// Register 37, Bit 6  0: push-pull
+    accelgyro.setInterruptLatch(1);// Register 37, Bit 5  0: 0: 50us-pulse, 1: latch-until-int-cleared
+    accelgyro.setInterruptLatchClear(0);// Register 37, Bit 4  0: 0=status-read-only
+    accelgyro.setFSyncInterruptLevel(1);// Register 37, Bit 3  0: active high
+    accelgyro.setFSyncInterruptEnabled(0);// Register 37, Bit 2  0: New FSYNC pin interrupt off
+    accelgyro.setI2CBypassEnabled(0);// Register 37, Bit 1  0: no direct access o econdary i2c
+    accelgyro.setClockOutputEnabled(0);// Register 37, Bit 0  0: no external clock
+//    accelgyro.setDLPFMode(3); // low-pass filter, 3: 44Hz
+    accelgyro.setDHPFMode(1); // high-pass filter, 2: 2.5Hz
+    accelgyro.setMotionDetectionThreshold(1);//20?
+    accelgyro.setMotionDetectionDuration(40); // millieconds, 40?
+    accelgyro.setAccelerometerPowerOnDelay(2); // 4+2ms
+    accelgyro.setMotionDetectionCounterDecrement(1);
+    accelgyro.setFreefallDetectionCounterDecrement(1);
+    accelgyro.setIntEnabled(0); // zero all
+    accelgyro.setIntMotionEnabled(1); // FINALLY
+    
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), onMotion, FALLING);
+    
     for(int i = 0; i < 3; i++){
       btn[i].attach(btnPin[i], INPUT_PULLUP);
       btn[i].interval(BTN_THROTTLE_DELAY);
@@ -54,6 +91,7 @@ void setup() {
 }
 
 void loop() {
+  if(isInterrupted){
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
     // some functions; more here: https://github.com/jrowberg/i2cdevlib/blob/master/Arduino/MPU6050/MPU6050.h
@@ -63,8 +101,12 @@ void loop() {
     //int16_t getRotationX(); int16_t getRotationY(); int16_t getRotationZ();
 
     processMouse(-gy, gz);
-    processBtns();
-    delay(15);
+    blinkState = !blinkState;
+    digitalWrite(LED_BUILTIN, blinkState);
+  }
+  isInterrupted = false;
+  processBtns();
+  delay(15);
 }
 
 void processMouse(int dX, int dY){
