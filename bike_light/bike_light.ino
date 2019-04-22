f3677d35b0dce9f01da712977f5fa2c690de91e9
mamert <mamert.k1@gmail.com>
2019-04-22 23:13:30 +0200

    bike light: added anmated PacMan shape

--- bike_light/bike_light.ino
+++ bike_light/bike_light.ino
@@ -8,13 +8,19 @@ int clkPin = 11;
 int csPin = 10;
 LedControl lc=LedControl(dataPin, clkPin, csPin, 1);
 
+int hshift = 0;
+
 //unsigned long refreshRate=100;
 
 ////////////////////
 
 void drawShape(byte shape[]){
-  for(int i=0; i<8; i++){
-    lc.setRow(0,i,shape[i]);
+  int hshift1 = hshift-8;
+  for(int i=0; i<8; i = i+1){
+    byte row = 0;
+    if(hshift+1 >= 0)
+      row = shape[hshift1+1];
+    lc.setRow(0,i,row);
   }
 }
 
@@ -24,10 +30,20 @@ void setup() {
   lc.shutdown(0,false); // wakeup?
   lc.clearDisplay(0);
   lc.setIntensity(0,1); // up to 15
-  drawShape(shapeEvilOtto);
 }
 
+void drawFrame(byte shape[]) {
+  drawShape(shape);
+  delay(100);
+}
 
 void loop() {
-  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  drawFrame(Pacman1);
+  drawFrame(Pacman2);
+  drawFrame(Pacman3);
+  drawFrame(Pacman4);
+  drawFrame(Pacman3);
+  drawFrame(Pacman2);
+  hshift = hshift+1;
+  hshift = hshift % 17;
 }
