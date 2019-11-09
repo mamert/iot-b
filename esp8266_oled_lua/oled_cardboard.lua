95d8c49e5bc3f29a7b7f2a249bdd476907d41a90
Kaay <kaay.null@gmail.com>
2019-11-09 17:59:13 +0100

    oled: moved main code to function

--- esp8266_oled_lua/oled_cardboard.lua
+++ esp8266_oled_lua/oled_cardboard.lua
@@ -24,42 +24,45 @@ wifi.setmode(wifi.NULLMODE)
 init_OLED()
 
 
+function drawItemDesc()
+	disp:firstPage()
+	repeat
+		local ypos=0
+		local xpos=0
+		set_font(fw.FONT.M)
+		disp:drawStr(xpos+1, ypos, "Cardboard Greave R")
+		disp:drawStr(xpos+2, ypos, "Cardboard Greave R")
 
-disp:firstPage()
-repeat
-	local ypos=0
-	local xpos=0
-	set_font(fw.FONT.M)
-	disp:drawStr(xpos+1, ypos, "Cardboard Greave R")
-	disp:drawStr(xpos+2, ypos, "Cardboard Greave R")
+
+		set_font(fw.FONT.S)
+		ypos=16
+		xpos=20
+		disp:drawStr(xpos, ypos, "of Cardboard Armor Set")
+		ypos=ypos+8+1
+		set_font(fw.FONT.M)
+		xpos=8
+		disp:drawStr(xpos, ypos, "+5")
+		set_font(fw.FONT.S)
+		disp:drawStr(xpos+18, ypos+3, "to Virginity Defense")
+		ypos=ypos+15
+		xpos=7
+		disp:drawStr(xpos+13, ypos, "Durability:")
+		disp:drawStr(xpos+70, ypos, "1")
+		disp:drawStr(xpos+71, ypos, "1")
+		disp:drawStr(xpos+81, ypos, "of")
+		disp:drawStr(xpos+96, ypos, "1")
+		disp:drawStr(xpos+97, ypos, "1")
+		ypos=ypos+8
+		xpos=15
+		disp:drawStr(xpos, ypos, "Required Strength: 0")
+		ypos=ypos+8
+		xpos=7
+		disp:drawStr(xpos, ypos, "Required Confidence: 50")
+	until disp:nextPage() == false
+
+	-- alt: "Ancient Cymbal"
+end
 
 
-	set_font(fw.FONT.S)
-	ypos=16
-	xpos=20
-	disp:drawStr(xpos, ypos, "of Cardboard Armor Set")
-	ypos=ypos+8+1
-	set_font(fw.FONT.M)
-	xpos=8
-	disp:drawStr(xpos, ypos, "+5")
-	set_font(fw.FONT.S)
-	disp:drawStr(xpos+18, ypos+3, "to Virginity Defense")
-	ypos=ypos+15
-	xpos=7
-	disp:drawStr(xpos+13, ypos, "Durability:")
-	disp:drawStr(xpos+70, ypos, "1")
-	disp:drawStr(xpos+71, ypos, "1")
-	disp:drawStr(xpos+81, ypos, "of")
-	disp:drawStr(xpos+96, ypos, "1")
-	disp:drawStr(xpos+97, ypos, "1")
-	ypos=ypos+8
-	xpos=15
-	disp:drawStr(xpos, ypos, "Required Strength: 0")
-	ypos=ypos+8
-	xpos=7
-	disp:drawStr(xpos, ypos, "Required Confidence: 50")
-until disp:nextPage() == false
-
--- alt: "Ancient Cymbal"
-
+drawItemDesc()
 
