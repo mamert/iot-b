e13e5c669bac230ff0c3e08bde6b5d3dd4301954
Kaay <kaay.null@gmail.com>
2019-11-14 16:11:26 +0100

    oled thing: remove filelist from boot (for plotting)

--- esp8266_oled_lua/init.lua
+++ esp8266_oled_lua/init.lua
@@ -1,6 +1,3 @@
-for k,v in pairs(file.list()) do  print(k .. "\t" .. v)end -- print SOMETHING for ESPlorer's sake
-
-
 tmr.create():alarm(1000, tmr.ALARM_SINGLE, function()
 	dofile("main.lua")
 end)
