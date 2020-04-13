f80aa15eb7d75ddf2534bc1f86c07bdf2476c6f9
Kaay <kaay.null@gmail.com>
2020-04-13 18:43:03 +0200

    ps2 touchscreen: work as USB mouse

--- ps2_touchscreen/ps2_touchscreen.ino
+++ ps2_touchscreen/ps2_touchscreen.ino
@@ -1,18 +1,20 @@
 #include <ps2.h>
 
 PS2 mouse(A2, A3);
-unsigned int maxx, minx, maxy, miny, maxz, minz;
+#include <Mouse.h>
+//#include <AltSoftSerial.h>
 
-byte mstat1;
-byte mstat2;
-byte mxy;
-byte mx;
-byte my;
-byte mz;
+char mstat1;
+char mstat2;
+char mxy;
+char mx;
+char my;
+char mz;
 
 
 unsigned int cx, cy;
 
+//AltSoftSerial altSerial;
 
 
 void kaoss_init()
@@ -23,7 +25,8 @@ void kaoss_init()
   mouse.read();  // blank */
   mouse.write(0xf0);  // remote mode
   mouse.read();  // ack
-  delayMicroseconds(100);
+  /* // advanced settings
+  delayMicroseconds(10);
   mouse.write(0xe8);
   mouse.read();  // ack byte
   mouse.write(0x03); // x1  ( x1 * 64  +  x2 * 16  +  x3 * 4  +  x4   == modebyte )
@@ -43,13 +46,14 @@ void kaoss_init()
   mouse.write(0xf3); // set samplerate 20 (stores mode)
   mouse.read();  // ack byte
   mouse.write(0x14);
-  mouse.read();  // ack byte
+  mouse.read();  // ack byte */
   delayMicroseconds(100);
 }
 
 void setup()
 {
   Serial.begin(115200);
+    // altSerial.begin(9600); // TODO: altSerial.begin(38400);
   
   Serial.println("setup 1");
   kaoss_init();
@@ -60,27 +64,45 @@ void setup()
 void loop()
 {
 //  Serial.println("cp1");
-  mouse.write(0xeb);
-  mouse.read();
+  mouse.write(0xeb); // req data
+  mouse.read(); // ignore ack
 
   mstat1 = mouse.read();
+  /* advanced
   mxy = mouse.read();
-  mz = mouse.read();
+  mz = mouse.read(); // starts detecting even millimeters above surface
   mstat2 = mouse.read();
+  */
   mx = mouse.read();
-  my = mouse.read();
+  my = -mouse.read();
 
 
-//  Serial.println("cp2");
+//  absolute values
   // collect the bits for x and y
-  cx = (((mstat2 & 0x10) << 8) | ((mxy & 0x0F) << 8) | mx );
-  cy = (((mstat2 & 0x20) << 7) | ((mxy & 0xF0) << 4) | my );
+//  cx = (((mstat2 & 0x10) << 8) | ((mxy & 0x0F) << 8) | mx );
+//  cy = (((mstat2 & 0x20) << 7) | ((mxy & 0xF0) << 4) | my );
+//  Serial.print(cx);
+//  Serial.print(" ");
+//  Serial.println(cy);
+    if(/* mz > 30 && */(mx != 0 || my != 0)){
+      Mouse.move(mx, my);
+//      altSerial.print("$M 0 ");
+//      altSerial.print(mx);
+//      altSerial.print(" ");
+//      altSerial.print(my);
+//      altSerial.print(" 0\n");
+    }
 
-  
-  Serial.print(cx);
-  Serial.print(" ");
-  Serial.println(cy);
+  /* advaced
+  Serial.print("\tXY=");
+  Serial.print(mxy, DEC);
+  Serial.print("\tZ=");
+  Serial.print(mz, DEC);
+  */
+//  Serial.print("\tX=");
+//  Serial.print(mx, DEC);
+//  Serial.print("\tY=");
+//  Serial.print(my, DEC);
 //  Serial.println();
-  delay(60);
-//  delay(20);
+  delay(10);
 }
